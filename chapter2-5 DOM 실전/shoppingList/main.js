468b81e02f28c665cb9d273b3b12b3adad2d98b0
TaehwanGo <gth1123@naver.com>
2020-12-16 05:02:09 +0900

    Tony's Shopping list app

---
+++ chapter2-5 DOM 실전/shoppingList/main.js
@@ -0,0 +1,48 @@
+const inputForm = document.querySelector("#jsInputForm");
+const inputText = document.querySelector(".input_item");
+const list = document.querySelector(".list");
+const plusBtn = document.querySelector(".plusBtn");
+
+function handlePlusBtn(){
+    makeItem();
+}
+
+function removeItem() {
+    // console.log(this);
+    this.parentNode.remove();
+}
+
+function makeItem() {
+    const textFromInput = inputText.value;
+    const textToSpan = document.createElement("span");
+    textToSpan.textContent = textFromInput;
+    // console.log(textToSpan);
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-trash-alt")
+    icon.addEventListener("click", removeItem);
+
+    const itemContainer = document.createElement("li");
+    itemContainer.setAttribute("class", "list_item");
+    // itemContainer.innerHTML = `
+    //     ${textToSpan.outerHTML}
+    //     <i class="fas fa-trash-alt"></i>
+    // `;    
+    itemContainer.appendChild(textToSpan);
+    itemContainer.appendChild(icon);
+
+    list.appendChild(itemContainer);
+    inputText.value = "";
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    makeItem();
+}
+
+
+function init() {
+    inputForm.addEventListener("submit", handleSubmit);
+    plusBtn.addEventListener("click", handlePlusBtn);
+}
+
+init();
